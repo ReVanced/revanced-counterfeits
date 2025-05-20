4f4f00cd98a5a2d75e32873abf333e76d149ddca
madkarmaa <madkarma99@gmail.com>
2025-05-20 17:47:07 +0200

    refactor: add spaces

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,7 @@
 		src="https://www.googletagmanager.com/ns.html?id={RV_GOOGLE_TAG_MANAGER_ID}"
 		height="0"
 		width="0"
-		style="display:none; visibility:hidden"
+		style="display: none; visibility: hidden"
 		title="Google Tag Manager"
 	></iframe>
 </noscript>
