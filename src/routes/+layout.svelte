a4f09e357988dd3766b640c9e0c180855d25c094
madkarmaa <madkarma99@gmail.com>
2025-05-20 17:42:13 +0200

    feat: add gtag manager

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { RV_GOOGLE_TAG_MANAGER_ID } from '$env/static/public';
+
+	type Props = { children: Snippet };
+	let { children }: Props = $props();
+
+	$effect(() => {
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag(...args: any[]) {
+			window.dataLayer.push(...args);
+		}
+
+		gtag('js', new Date());
+		gtag('config', RV_GOOGLE_TAG_MANAGER_ID);
+
+		const script = document.createElement('script');
+		script.src = `https://www.googletagmanager.com/gtm.js?id=${RV_GOOGLE_TAG_MANAGER_ID}`;
+		document.head.append(script);
+	});
+</script>
+
+<noscript>
+	<iframe
+		src="https://www.googletagmanager.com/ns.html?id={RV_GOOGLE_TAG_MANAGER_ID}"
+		height="0"
+		width="0"
+		style="display:none; visibility:hidden"
+		title="Google Tag Manager"
+	></iframe>
+</noscript>
+
+{@render children()}
