88aa9f98509b8a6aa2ef20ad38c40ef118045e5a
madkarmaa <madkarma99@gmail.com>
2025-05-20 19:00:43 +0200

    fix: handle not existing routes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
@@ -11,6 +12,12 @@ const config = {
 		},
 		env: {
 			publicPrefix: 'RV'
+		},
+		prerender: {
+			handleHttpError: ({ status, message }) => {
+				if (status === 404) return;
+				throw new Error(message);
+			}
 		}
 	}
 };
