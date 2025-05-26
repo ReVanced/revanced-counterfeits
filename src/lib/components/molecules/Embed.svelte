e9b4a9860214a33e882e51b92dc21627c3f149ad
madkarmaa <madkarma99@gmail.com>
2025-05-26 19:58:15 +0200

    feat: finish embed component

--- src/lib/components/molecules/Embed.svelte
+++ src/lib/components/molecules/Embed.svelte
@@ -5,25 +5,25 @@
 
 <svelte:head>
 	<meta name="title" content={title} />
+	<meta property="og:title" content={title} />
+	<meta property="twitter:title" content={title} />
+
+	<meta property="og:type" content="website" />
+	<meta property="twitter:card" content="summary_large_image" />
+
 	{#if description}
 		<meta name="description" content={description} />
-	{/if}
-
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://metatags.io/" />
-	<meta property="og:title" content={title} />
-	{#if description}
 		<meta property="og:description" content={description} />
-	{/if}
-	<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
-
-	<!-- Twitter -->
-	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content="https://metatags.io/" />
-	<meta property="twitter:title" content={title} />
-	{#if description}
 		<meta property="twitter:description" content={description} />
 	{/if}
-	<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+	{#if websiteUrl}
+		<meta property="og:url" content={websiteUrl} />
+		<meta property="twitter:url" content={websiteUrl} />
+	{/if}
+
+	{#if image}
+		<meta property="og:image" content={image} />
+		<meta property="twitter:image" content={image} />
+	{/if}
 </svelte:head>
